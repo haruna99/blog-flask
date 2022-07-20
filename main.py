fa7f56abde0bd41face936015f4008b3c8010438
Haruna Ogweda <hogweda@gmail.com>
2022-07-20 03:49:23 -0500

    updated blog user interface

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-from flask import Flask, render_template
-from post import Post
+from flask import Flask, render_template, url_for
+import requests
 
 app = Flask(__name__)
-post = Post().posts
+
+response = requests.get("https://api.npoint.io/338826d1ef3417713ca3")
+posts = response.json()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html", posts=post)
+    return render_template("index.html", posts=posts)
 
 
-@app.route("/post/<int:post_id>")
-def single_post(post_id):
-    requested_post = None
-    for blog_post in post:
-        if blog_post["id"] == post_id:
-            requested_post = blog_post
-    return render_template("post.html", id=post_id, post=requested_post)
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    single_post = None
+    for new_post in posts:
+        if post_id == new_post["id"]:
+            single_post = new_post
+    return render_template("post.html", post=single_post)
 
 
 if __name__ == "__main__":
--- post.py
+++
@@ -1,9 +0,0 @@
-import requests
-blog_url = "https://api.npoint.io/d8ac0f18e2b7fc75e7d7"
-
-
-class Post:
-    def __init__(self):
-        response = requests.get(url=blog_url)
-        self.posts = response.json()
-
