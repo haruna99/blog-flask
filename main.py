69aae2e29d2d943bbedb68ebff9b48d57b57208f
Haruna Ogweda <hogweda@gmail.com>
2022-07-22 00:16:04 -0500

    updated blog contact form

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import requests
+from smtplib import SMTP
+import os
+
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 app = Flask(__name__)
 
@@ -12,9 +17,25 @@ def home():
     return render_template("index.html", posts=posts)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "GET":
+        success = False
+    if request.method == "POST":
+        data = request.form
+        name = data["username"]
+        email = data["email"]
+        message = data["message"]
+        phone = data["phone"]
+        with SMTP("smtp.mail.yahoo.com") as connection:
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg=f"Subject:Haruna's Blog\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}")
+        success = True
+    return render_template("contact.html", success=success)
 
 
 @app.route('/about')
